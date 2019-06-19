91a3d7138eecee2150eda07c772daa2ac64a3fd3
Nhat Phan <nhat.phan@ntworld.net>
2019-06-19 15:32:00 +0200

    Move builtin rules to a separated interface

--- src/commonMain/kotlin/net/ntworld/kotlin/validator/ValidatorRules.kt
+++ src/commonMain/kotlin/net/ntworld/kotlin/validator/ValidatorRules.kt
@@ -1,16 +1,13 @@
 package net.ntworld.kotlin.validator
 
-import net.ntworld.kotlin.validator.internal.RuleFactory
-import net.ntworld.kotlin.validator.rule.*
-import net.ntworld.kotlin.validator.rule.NumberComparison
+import net.ntworld.kotlin.validator.rule.Each
 import net.ntworld.kotlin.validator.rule.Optional
 import net.ntworld.kotlin.validator.rule.Required
-import net.ntworld.kotlin.validator.rule.StringLength
 import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
 
 @ValidatorRulesDsl
-interface ValidatorRules<T> {
+interface ValidatorRules<T>: BuiltinRules {
     // Builder feature ----------------------------------------------------
     @ValidatorRulesDsl
     infix fun extend(validator: Validator<in T>): ValidatorRules<T>
@@ -70,100 +67,4 @@ interface ValidatorRules<T> {
     infix fun <E, R : Collection<E?>> KProperty1<T, R?>.each(rule: Rule<E>): RuleBuilder<R> {
         return this.always(Optional()).and(Each(rule))
     }
-
-    // Builtin rules -------------------------------------------------
-
-    @RuleDsl
-    val exists: Rule<Any>
-        get() = RuleFactory.notNull
-
-    @RuleDsl
-    val required: Rule<Any>
-        get() = RuleFactory.required
-
-    @RuleDsl
-    val notNull: Rule<Any>
-        get() = RuleFactory.notNull
-
-    @RuleDsl
-    val notEmptyString: Rule<String>
-        get() = RuleFactory.notEmptyString
-
-    @RuleDsl
-    infix fun <V> pass(fn: (V?) -> Boolean): Rule<V> = Pass(fn)
-
-    @RuleDsl
-    infix fun <V> passes(fn: (V?) -> Boolean): Rule<V> = pass(fn)
-
-    @RuleDsl
-    fun <V> eq(value: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_EQUAL, value)
-
-    @RuleDsl
-    fun <V> equal(value: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_EQUAL, value)
-
-    @RuleDsl
-    fun <V> gt(min: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_GREATER_THAN, min)
-
-    @RuleDsl
-    fun <V> greaterThan(min: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_GREATER_THAN, min)
-
-    @RuleDsl
-    fun <V> gte(min: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_GREATER_THAN_OR_EQUAL, min)
-
-    @RuleDsl
-    fun <V> greaterThanOrEqual(min: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_GREATER_THAN_OR_EQUAL, min)
-
-    @RuleDsl
-    fun <V> lt(max: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_LESS_THAN, max)
-
-    @RuleDsl
-    fun <V> lessThan(max: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_LESS_THAN, max)
-
-    @RuleDsl
-    fun <V> lte(max: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_LESS_THAN_OR_EQUAL, max)
-
-    @RuleDsl
-    fun <V> lessThanOrEqual(max: V): Rule<V> where V : Number, V : Comparable<V> =
-        NumberComparison(OPERATOR_LESS_THAN_OR_EQUAL, max)
-
-    @RuleDsl
-    fun minLength(value: Int): Rule<String> = StringLength(OPERATOR_GREATER_THAN_OR_EQUAL, value)
-
-    @RuleDsl
-    fun maxLength(value: Int): Rule<String> = StringLength(OPERATOR_LESS_THAN_OR_EQUAL, value)
-
-    @RuleDsl
-    fun exactLength(value: Int): Rule<String> = StringLength(OPERATOR_LESS_THAN_OR_EQUAL, value)
-
-    @RuleDsl
-    fun <V : CharSequence> passRegex(pattern: String): Rule<V> = PassRegex(kotlin.text.Regex(pattern))
-
-    @RuleDsl
-    fun <V : CharSequence> passRegex(pattern: String, option: RegexOption): Rule<V> = PassRegex(Regex(pattern, option))
-
-    @RuleDsl
-    fun <V : CharSequence> passRegex(pattern: String, option: Set<RegexOption>): Rule<V> =
-        PassRegex(Regex(pattern, option))
-
-    @RuleDsl
-    fun <V : CharSequence> matches(pattern: String): Rule<V> = passRegex(pattern)
-
-    @RuleDsl
-    fun <V : CharSequence> matches(pattern: String, option: RegexOption): Rule<V> = passRegex(pattern, option)
-
-    @RuleDsl
-    fun <V : CharSequence> matches(pattern: String, option: Set<RegexOption>): Rule<V> = passRegex(pattern, option)
-
-    @RuleDsl
-    infix fun <E, V : Collection<E?>> each(rule: Rule<E>): Rule<V> = Each(rule)
-
 }
