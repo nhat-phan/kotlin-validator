6b3de92182b18f603c75f8d7f382c758f2a39c4e
Nhat Phan <nhat.phan@ntworld.net>
2019-06-24 13:08:00 +0200

    Add missing project folders, add integration tests for NotEmptyString and NotNull

--- src/commonTest/kotlin/net.ntworld.kotlin.validator/rule/NotEmptyStringTest.kt
+++ src/commonTest/kotlin/net.ntworld.kotlin.validator/rule/NotEmptyStringTest.kt
@@ -1,5 +1,7 @@
 package net.ntworld.kotlin.validator.rule
 
+import net.ntworld.kotlin.validator.MESSAGE_NOT_EMPTY_STRING
+import net.ntworld.kotlin.validator.validator
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,19 +11,39 @@ class NotEmptyStringTest {
         val valid: Boolean
     )
 
+    private val dataset = listOf(
+        TestData(value = null, valid = false),
+        TestData(value = "", valid = false),
+        TestData(value = " ", valid = false),
+        TestData(value = "a ", valid = true),
+        TestData(value = "  a", valid = true),
+        TestData(value = "123", valid = true)
+    )
+
     @Test
     fun testPasses() {
-        val dataset = listOf(
-            TestData(value = null, valid = false),
-            TestData(value = "", valid = false),
-            TestData(value = " ", valid = false),
-            TestData(value = "a ", valid = true),
-            TestData(value = "  a", valid = true),
-            TestData(value = "123", valid = true)
-        )
         val rule = NotEmptyString()
         for (data in dataset) {
             assertEquals(data.valid, rule.passes("test", data.value), data.toString())
+        }
+    }
+
+    @Test
+    fun testIntegration() {
+        data class Data(val value: String?)
+
+        val validator = validator<Data> {
+            Data::value { rule = notEmptyString }
+        }
+        for (data in dataset) {
+            val result = validator.validate(Data(data.value))
+            assertEquals(data.valid, result.isValid)
+            if (!data.valid) {
+                assertEquals(
+                    setOf(MESSAGE_NOT_EMPTY_STRING.replace(":attribute", "value")),
+                    result.errors.get("value")
+                )
+            }
         }
     }
 }
--- src/commonTest/kotlin/net.ntworld.kotlin.validator/rule/NotNullTest.kt
+++ src/commonTest/kotlin/net.ntworld.kotlin.validator/rule/NotNullTest.kt
@@ -1,5 +1,7 @@
 package net.ntworld.kotlin.validator.rule
 
+import net.ntworld.kotlin.validator.MESSAGE_NOT_NULL
+import net.ntworld.kotlin.validator.validator
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,19 +11,39 @@ class NotNullTest {
         val valid: Boolean
     )
 
+    private val dataset = listOf(
+        TestData(value = null, valid = false),
+        TestData(value = "", valid = true),
+        TestData(value = " ", valid = true),
+        TestData(value = "a ", valid = true),
+        TestData(value = "  a", valid = true),
+        TestData(value = "123", valid = true)
+    )
+
     @Test
     fun testPasses() {
-        val dataset = listOf(
-            TestData(value = null, valid = false),
-            TestData(value = "", valid = true),
-            TestData(value = " ", valid = true),
-            TestData(value = "a ", valid = true),
-            TestData(value = "  a", valid = true),
-            TestData(value = "123", valid = true)
-        )
         val rule = NotNull()
         for (data in dataset) {
             assertEquals(data.valid, rule.passes("test", data.value), data.toString())
+        }
+    }
+
+    @Test
+    fun testIntegration() {
+        data class Data(val value: Any?)
+
+        val validator = validator<Data> {
+            Data::value { rule = notNull }
+        }
+        for (data in dataset) {
+            val result = validator.validate(Data(data.value))
+            assertEquals(data.valid, result.isValid)
+            if (!data.valid) {
+                assertEquals(
+                    setOf(MESSAGE_NOT_NULL.replace(":attribute", "value")),
+                    result.errors.get("value")
+                )
+            }
         }
     }
 }
